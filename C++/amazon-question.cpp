fa11fcce6915e0ba5121c942f298b17c022fcd9b
vijay62633 <ajaym6708@gmail.com>
2025-02-27 12:19:16 +0530

    first commit

---
+++ C++/amazon-question.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  str websitename;
+  cout<<"Enter websitename:";
+  cin>>websitename;
+
+  if(websitename=="A")
+
+return 0;
+}
---
+++ C++/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+cout<<sizeof(int)<<endl;
+  // cout<<sizeof(float)<<endl;
+  // cout<<sizeof(bool)<<endl;
+  // cout<<sizeof(char)<<endl;
+  // cout<<sizeof(double)<<endl;
+  // cout<<sizeof(long)<<endl;
