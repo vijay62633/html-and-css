35e329b0d298ae0d32a6ff70a02ccf5c780f1722
vijay62633 <ajaym6708@gmail.com>
2025-02-28 11:52:41 +0530

    Updated

--- C++/amazon-question.cpp
+++ C++/amazon-question.cpp
@@ -2,11 +2,41 @@
 using namespace std;
 int main()
 {
-  str websitename;
+  string websitename;
   cout<<"Enter websitename:";
   cin>>websitename;
 
-  if(websitename=="A")
+  if(websitename=="Amazon")
+  {
+    cout<<"Product or Service:";
+    cin>>websitename;
+    if(websitename=="Product")
+    {
+      cout<<"Shoes or Clothes:";
+      cin>>websitename;
+      if(websitename=="Shoes")
+      {
+        cout<<"1500";
+      }
+      else if(websitename=="Clothes")
+      {
+        cout<<"2500";
+      }
+      else{
+        cout<<"Invalid Input";
+      }
+    }
+    else if(websitename=="Service")
+    {
+      cout<<"Not Available";
+    }
+    else{
+      cout<<"Inavalid";
+    }
+  }
+  else{
+    cout<<"Inavalid Input";
+  }
 
 return 0;
 }
