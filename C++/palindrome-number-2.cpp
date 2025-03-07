a92bd2b325a26adf3b4eb3f0c1a25fdcfe68b336
vijay62633 <ajaym6708@gmail.com>
2025-03-07 11:00:36 +0530

    march7

--- C++/palindrome-number-2.cpp
+++ C++/palindrome-number-2.cpp
@@ -3,16 +3,22 @@ using namespace std;
 int main()
 {
 
-  int num,rev=0;
+  int num,rev=0,comp;
   cout<<"Enter a number:";
   cin>>num;
+  comp=num;
 
   while(num>0){
     int digit=num%10;
     rev=rev*10+digit;
     num=num/10;
   }
-  cout<<rev;
+  if(rev==comp){
+    cout<<"Palindrome number";
+  }
+  else{
+    cout<<"Not a palindrome number";
+  }
 
 return 0;
 }
