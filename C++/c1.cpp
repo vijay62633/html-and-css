b443be4cb19562523a5e84c3f90f151f0b7b6ab0
vijay62633 <ajaym6708@gmail.com>
2025-03-27 11:37:35 +0530

    27march

--- C++/c1.cpp
+++ C++/c1.cpp
@@ -2,14 +2,7 @@
 using namespace std;
 int main()
 {
-  int a,b,c;
-  cout<<"enter three numbers:";
-  cin>>a>>b>>c;
-
-  (a>b && a>c)?cout<<a:(b>a && b>c) ?cout<<b:cout<<c;
-  cout<<a;
-  cout<<b;
-  cout<<c;
+  
 
 
 return 0;
