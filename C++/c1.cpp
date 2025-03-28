0f0a6de21321fb214b1719a84548a603a4aa122e
vijay62633 <ajaym6708@gmail.com>
2025-03-28 10:25:10 +0530

    28march

--- C++/c1.cpp
+++ C++/c1.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 int main()
 {
-  
+  char val;
+  cout<<"Enter character:";
+  cin>>val;
 
+  (val=='a'||val=='e'||val=='i'||val=='o'||val=='u')? cout<<"vowel": cout<<"consonant";
 
 return 0;
 }
