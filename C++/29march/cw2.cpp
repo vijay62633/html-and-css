8d2113249e821a084d2fcdbda3d72393a6800893
vijay62633 <ajaym6708@gmail.com>
2025-03-29 23:53:08 +0530

    29

---
+++ C++/29march/cw2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int num;
+  cout<<"Enter 0-4 for Poor\nEnter 5-6 for Average\nEnter 7-8 for Good\nEnter 9-10 for Excellent\n\nEnter a Number:";
+
+
+return 0;
+}
