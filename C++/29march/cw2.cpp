0c0f0af7401bb65f249a4d6fd96dc975963920ef
vijay62633 <ajaym6708@gmail.com>
2025-03-29 23:55:08 +0530

    29

--- C++/29march/cw2.cpp
+++ C++/29march/cw2.cpp
@@ -4,6 +4,7 @@ int main()
 {
   int num;
   cout<<"Enter 0-4 for Poor\nEnter 5-6 for Average\nEnter 7-8 for Good\nEnter 9-10 for Excellent\n\nEnter a Number:";
+  cout<<"Enter 0-4 for Poor\nEnter 5-6 for Average\nEnter 7-8 for Good\nEnter 9-10 for Excellent\n\nEnter a Number:";
 
 
 return 0;
